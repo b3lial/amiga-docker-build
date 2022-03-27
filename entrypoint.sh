e68831512d902ad3476952226b7e66941f60c0ae
Dr. Christian Ammann <christian.ammann@phobosys.de>
2022-03-27 15:38:30 +0200

    added error checks

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+if [ -z "${BUILD_RESULT}" ]; then
+  echo aborting because \$BUILD_RESULT is not set
+  exit
+fi
+if [ ! -d /build ]; then 
+  echo aborting because /build directory is empty
+  exit
+fi
+
 export PATH=$PATH:/opt/vbcc/bin
 cp -r /build /tmp/build
 cd /tmp/build
