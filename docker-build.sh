7a522f3afd7dafac92b012225b79787ed46eed44
Christian Ammann <christian.ammann@phobosys.de>
2025-10-03 21:33:43 +0200

    updated docs

---
+++ docker-build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+VERSION=`cat VERSION`          
+IMAGE=amiga-gcc-builder
+
+# download compiler, assembler and linker
+if [ ! -d "amiga-gcc" ]
+then
+  echo Downloading GCC distribution
+  git clone https://franke.ms/git/bebbo/amiga-gcc.git
+fi
+if [ ! -f "vasm.tar.gz" ]
+then
+  echo Downloading vasm sources
+  curl https://amiga-docker-build.s3.eu-central-1.amazonaws.com/vasm.tar.gz -o vasm.tar.gz
+fi
+if [ ! -f "vlink.tar.gz" ]
+then
+  Downloading vlink sources
+  curl https://amiga-docker-build.s3.eu-central-1.amazonaws.com/vlink.tar.gz -o vlink.tar.gz
+fi
+
+echo "creating $IMAGE, version: $VERSION"
+docker build -t $IMAGE:latest .
+docker tag $IMAGE:latest $IMAGE:$VERSION
